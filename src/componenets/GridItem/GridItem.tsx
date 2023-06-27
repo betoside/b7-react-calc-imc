69f28a72f7c5aa8fe9d8ed63a4ad137927b5355a
Betoside <betoside@gmail.com>
2023-06-27 17:13:35 -0300

    first commit

---
+++ src/componenets/GridItem/GridItem.tsx
@@ -0,0 +1,39 @@
+import { Level } from "../../helpers/imc";
+import styles from './GridItem.module.css';
+import upImage from '../../assets/up.png';
+import downImage from '../../assets/down.png';
+
+type Props = {
+    item: Level;
+}
+
+export const GridItem = ( {item}: Props ) => {
+    return (
+        <div className={styles.main} style={{ backgroundColor: item.color }}>
+            <div className={styles.gridIcon}>
+
+                {/* forma 1: */}
+                {item.icon === 'up' && <img src={upImage} alt='' width='30' />}
+                {item.icon === 'down' && <img src={downImage} alt='' width='30' />}
+
+                {/* forma 2 */}
+                {/* <img src={ item.icon === 'up' ? upImage : downImage } alt="" width='30' /> */}
+            </div>
+            <div className={styles.gridTitle}>{item.title}</div>
+
+            {item.yourImc &&
+                <div className={styles.yourImc}>
+                    Seu IMC é: <br /> {item.yourImc}
+                </div>
+            }
+
+            <div className={styles.gridInfo}>
+                <>
+                    IMC está entre <br />
+                    <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+                </>
+            </div>
+        </div>
+    );
+    
+}
